573188addda9b3e74d5c26b1688775a8ec950c52
armaango <armaan.goyal@gmail.com>
2016-07-26 19:23:38 -0400

    Initial Commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+        label.text = "Hello World!"
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
